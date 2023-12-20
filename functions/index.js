cc9651b93f762ee80837aa9fd934aeb75f45da0d
Johnny Ji <johnny@johnnyji.com>
2023-12-19 16:00:50 -0800

    Migrate to OpenAI v4 and using gpt3.5-turbo

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const {Configuration, OpenAIApi} = require("openai");
+const OpenAI = require("openai");
 const functions = require("firebase-functions/v2");
 
 const openAIKey = functions.params.defineSecret("OPENAI_KEY");
@@ -15,6 +15,18 @@ const getSystemWording = (system) => {
   if (system === "EXCEL") return "an Excel";
   if (system === "SHEETS") return "a Google Sheets";
   if (system === "AIRTABLE") return "an Airtable";
+};
+
+const performCompletion = (openai, question) => {
+  return openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: question}],
+    temperature: 0,
+    max_tokens: 1000,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
 };
 
 exports.getFirebaseConfig = functions.https.onRequest(
@@ -63,30 +75,30 @@ exports.explain = functions.https.onRequest(
       secrets: [openAIKey],
     },
     (req, res) => {
-      const configuration = new Configuration({
+      const openai = new OpenAI({
         apiKey: openAIKey.value(),
       });
-      const openai = new OpenAIApi(configuration);
+
       const {
         data: {prompt, system},
       } = req.body;
 
-      openai
-          .createCompletion({
-            model: "text-davinci-003",
-            prompt: `Explain to me what this ${getSystemWording(
-                system,
-            )} is doing in plain english:\n\n${prompt}`,
-            temperature: 0,
-            max_tokens: 1000,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0,
-          })
+      const question = `
+        You are an AI who can read and explain ${getSystemWording(
+      system,
+  )} formulas, here are some rules you must follow:\n\n
+
+        1. Explain to the user in plain english what the formula is doing in plain english.\n
+        2. If the prompt is no a valid formula, ask the user to try again with a valid prompt\n\n
+
+        Here is the prompt:\n\n ${prompt}
+      `;
+
+      performCompletion(openai, question)
           .then((completion) => {
             res.send({
               status: 200,
-              data: completion.data.choices[0],
+              data: {text: completion.choices[0].message.content},
             });
           })
           .catch((err) => {
@@ -102,36 +114,36 @@ exports.generate = functions.https.onRequest(
     {
     // This must be defined everytime, can't be in a variable
       cors: [
+        "http://localhost:3000",
         "https://excelformulator.com",
         "https://excelbuilder.firebaseapp.com",
       ],
       secrets: [openAIKey],
     },
     (req, res) => {
-      const configuration = new Configuration({
+      const openai = new OpenAI({
         apiKey: openAIKey.value(),
       });
-      const openai = new OpenAIApi(configuration);
       const {
         data: {prompt, system},
       } = req.body;
 
-      openai
-          .createCompletion({
-            model: "text-davinci-003",
-            prompt: `Generate me ${getSystemWording(
-                system,
-            )} formula for:\n\n${prompt}`,
-            temperature: 0,
-            max_tokens: 1000,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0,
-          })
+      const question = `
+        You are an AI who can generate ${getSystemWording(
+      system,
+  )} formulas for the given prompt, here are some rules you must follow:\n\n
+
+        1. Do not return any other text except for the formula.\n
+        2. If the prompt doesn't make sense, ask the user to try again with a valid prompt\n\n
+
+        Here is the prompt:\n\n ${prompt}
+      `;
+
+      performCompletion(openai, question)
           .then((completion) => {
             res.send({
               status: 200,
-              data: completion.data.choices[0],
+              data: {text: completion.choices[0].message.content},
             });
           })
           .catch((err) => {
